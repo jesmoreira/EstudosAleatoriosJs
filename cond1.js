985b74f3df5c58e8d1eef485ed2c1a9a79097bde
jesmoreira <jessicamakani@gmail.com>
2022-02-21 16:36:36 -0300

    primeiro commit

---
+++ cond1.js
@@ -0,0 +1,6 @@
+var vel = 12.5
+console.log(`A velocidade do seu carro é ${65.5} kh`)
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade e foi multado.')
+}
+//condição simples 
---
+++ repetições.js
@@ -0,0 +1,5 @@
+var c = -12
+while (c <= 50) { //estrutura de repetição com teste logico no inicio
+    console.log(`Passo ${c}`)
+    c++
+}
